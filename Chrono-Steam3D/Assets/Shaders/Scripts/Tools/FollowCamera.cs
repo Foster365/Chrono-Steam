04f15578285665fcbb0c1e180e27391bdd98cdfc
uriel_mr_friky <ventas-u@hotmail.com>
2021-10-19 18:39:05 -0300

    tutorial ui sistem implemented

---
+++ Chrono-Steam3D/Assets/Shaders/Scripts/Tools/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    private GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+    }
+}
---
+++ Chrono-Steam3D/Assets/Shaders/Scripts/Tools/OpacityTimer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OpacityTimer : MonoBehaviour
+{
+    [SerializeField] private float _time;
+    [SerializeField] private SpriteRenderer [] _images;
+    [SerializeField] private TextMeshProUGUI [] _texts;
+    private float _currentTime;
+    private void Awake()
+    {
+        _currentTime = _time;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentTime <= 0)
+        {
+            foreach (var item in _images)
+            {
+                if(item.color.a < 255)
+                {
+                    item.color += new Color (0,0,0, 0.1f);
+                }
+                else
+                    return;
+            }
+
+            foreach (var item in _texts)
+            {
+                if (item.color.a < 255)
+                {
+                    item.color += new Color(0, 0, 0, 0.1f);
+                }
+                else
+                    return;
+            }
+        }else
+        {
+            foreach (var item in _images)
+            {
+                if (item.color.a > 0)
+                {
+                    item.color -= new Color(0, 0, 0, 0.1f);
+                }
+                else
+                    return;
+            }
+
+            foreach (var item in _texts)
+            {
+                if (item.color.a > 0)
+                {
+                    item.color -= new Color(0, 0, 0, 0.1f);
+                }
+                else
+                    return;
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _currentTime -= Time.deltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _currentTime = _time;
+        }
+    }
+}
