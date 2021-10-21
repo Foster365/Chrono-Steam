bd91e19f520204d28e71782035c86a40307f3fe1
uriel_mr_friky <ventas-u@hotmail.com>
2021-10-21 17:32:19 -0300

    coments and save

--- Chrono-Steam3D/Assets/Scripts/Tools/CurveSpeed.cs
+++ Chrono-Steam3D/Assets/Scripts/Tools/CurveSpeed.cs
@@ -9,18 +9,21 @@ public class CurveSpeed : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        // save the initial speed for reset later
        _initialSpeed = stateInfo.speedMultiplier;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        // set de speed by parameter multiplaier baced on animation time 
         animator.SetFloat("SpeedMultiplaier",  _speedCurve.Evaluate(stateInfo.normalizedTime));
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //reset speed multiplayer
         animator.SetFloat("SpeedMultiplaier",_initialSpeed);
     }
 
