c7e7208ec7945c621175b698795341b5175c6abd
Foster365 <31601427+Foster365@users.noreply.github.com>
2021-11-04 01:49:56 -0300

    Fall Collider, Boss Health UI, Hit Count UI.
    Fall collider fixed.
    Hit Count UI fixed.
    Boss Health UI -> Images setted up in canvas.
    -> Boss GameObject (Trigger) With Script (UI Enabler/Disabler) Not working properly.

---
+++ Chrono-Steam3D/Assets/Scripts/UI/Boss_UI_Trigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_UI_Trigger : MonoBehaviour
+{
+    [SerializeField] Image bossFillAmountImage;
+    LayerMask playerReference;
+
+    private void Start()
+    {
+        playerReference = GameObject.FindGameObjectWithTag("Player").layer;
+    }
+
+    private void Update()
+    {
+        CheckUIEnable();
+    }
+
+    bool CheckForPlayer(LayerMask playerRef)
+    {
+        Collider[] playerDetectionArea = Physics.OverlapSphere(transform.position, 10, playerReference);
+        if (playerDetectionArea!= null) return true;
+        else return false;
+    }
+
+    void CheckUIEnable()
+    {
+        if (CheckForPlayer(playerReference)) bossFillAmountImage.enabled = true;
+        else bossFillAmountImage.enabled = false;
+    }
+}
