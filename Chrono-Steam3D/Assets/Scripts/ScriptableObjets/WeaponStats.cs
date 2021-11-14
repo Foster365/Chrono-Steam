00c708dd8feda600bcd21869f0ae0b1c67733a93
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2021-11-14 19:19:29 -0300

    Damage Frame.
    Damage Frame showing according to health.

--- Chrono-Steam3D/Assets/Scripts/ScriptableObjets/WeaponStats.cs
+++ Chrono-Steam3D/Assets/Scripts/ScriptableObjets/WeaponStats.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "ScriptableObject/Weapon/WeaponStats", order = 1)]
 public class WeaponStats : ScriptableObject
@@ -12,6 +13,7 @@ public class WeaponStats : ScriptableObject
     [SerializeField] private float coolDown = 0;
     [SerializeField] private float _EspExeCd = 3;
     [SerializeField] private int _espDamage = 0;
+    [SerializeField] Image weaponIconImage;
 
     //[SerializeField] private IAttack attStrategy;
     public int AttDamage { get => attDamage;}
--- Chrono-Steam3D/Assets/Scripts/UI/Damage_Frame_UI.cs
+++ Chrono-Steam3D/Assets/Scripts/UI/Damage_Frame_UI.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage_Frame_UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Image playerDamageFrameImage;
+
+    float playerHealth;
+    float playerMaxHealth;
+
+    private void Awake()
     {
-        
+
+        playerDamageFrameImage = GameObject.FindGameObjectWithTag("Health_Frame_UI").gameObject.GetComponent<Image>();
+
+        if (playerDamageFrameImage == null) Debug.Log("Player Health Frame UI is null in Awake function");
+
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        playerHealth = GameManager.Instance.PlayerInstance.GetComponent<Player_Controller>().Life_Controller.CurrentLife;
+        playerMaxHealth = GameManager.Instance.PlayerInstance.GetComponent<Player_Controller>().PlayerStats.MaxLife;
+
+        var tempColor = playerDamageFrameImage.color;
+        tempColor.a = -(playerHealth/playerMaxHealth)+1;
+        playerDamageFrameImage.color = tempColor;
+
+        Debug.Log("Fill Amount is: " + tempColor.a);
+
+
     }
 }
